63ac4e46cc7857b1f75d5f96984b5d2637de1463
Prajkta <prajaktabhamare120@gmail.com>
2019-11-28 23:38:25 +0530

    initial version

---
+++ src/app/product.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  url = 'http://localhost:9898/product'
+ 
+
+  constructor(private http: HttpClient) { }
+
+  getProducts() {
+    return this.http.get(this.url)
+  }
+}
